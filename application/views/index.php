6d4b6076ada775cb6d5a11e3dae0d9e72feda86c
Agungmaulana27 <agungmaulana397@gmail.com>
2022-02-08 07:18:58 -0800

    first commit

---
+++ application/views/index.php
@@ -0,0 +1,188 @@
+<!--================Home Banner Area =================-->
+<section class="home_banner_area">
+    <div class="banner_inner d-flex align-items-center">
+        <div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background="">
+        </div>
+        <div class="container">
+            <div class="banner_content text-center">
+                <h3 data-aos="fade-up" data-aos-duration="1600">SMA APA AJA <br /> NEGERI 12</h3>
+                <p data-aos="fade-up" data-aos-duration="1900">Jl. Salembaran Kampung Melayu Timur Rt. 001/022 Kec. Teluknaga Banten 15510 </p>
+                <a data-aos="fade-up" data-aos-duration="2000" class="main_btn" href="<?= base_url('user/registration') ?>#registration">Bergabung Sekarang <span class="lnr lnr-arrow-right"></span></a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ================End Home Banner Area ================= -->
+
+<!--================Finance Area =================-->
+<section class="finance_area">
+    <div class="container">
+        <div class="finance_inner row">
+            <div class="col-lg-3 col-sm-6">
+                <div class="finance_item">
+                    <div data-aos="fade-right" data-aos-duration="1600" class="media">
+                        <div class="d-flex">
+                            <i class="lnr lnr-users"></i>
+                        </div>
+                        <div class="media-body">
+                            <h5>Admin, User & Guru Data Management</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="finance_item">
+                    <div data-aos="fade-right" data-aos-duration="1800" class="media">
+                        <div class="d-flex">
+                            <i class="lnr lnr-file-empty"></i>
+                        </div>
+                        <div class="media-body">
+                            <h5>Dokumentasi lengkap & Jelas</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div data-aos="fade-right" data-aos-duration="2000" class="col-lg-3 col-sm-6">
+                <div class="finance_item">
+                    <div class="media">
+                        <div class="d-flex">
+                            <i class="lnr lnr-camera-video"></i>
+                        </div>
+                        <div class="media-body">
+                            <h5>E-Learning Berbasis Video dan Disqus</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div data-aos="fade-right" data-aos-duration="2200" class="col-lg-3 col-sm-6">
+                <div class="finance_item">
+                    <div class="media">
+                        <div class="d-flex">
+                            <i class="lnr lnr-tag"></i>
+                        </div>
+                        <div class="media-body">
+                            <h5>Gratis dengan Lisensi MIT Open-Source</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--================End Finance Area =================-->
+
+
+
+<!--================Courses Area =================-->
+<section class="courses_area p_40">
+    <div class="container">
+        <div class="main_title">
+            <h2 data-aos="fade-up" data-aos-duration="1600">Pelajaran Yang Tersedia</h2>
+            <p data-aos="fade-up" data-aos-duration="1800">Merciful revaluation burying love ultimate value inexpedient ubermensch. Holiest madness victorious morality hope endless christian madness. Love dead fearful transvaluation marvelous. Oneself right ideal abstract endless faith deceptions zarathustra grandeur law ubermensch free.</p>
+        </div>
+        <div class="row courses_inner">
+            <div class="col-lg-9">
+                <div class="grid_inner">
+                    <div class="grid_item wd55">
+                        <div class="courses_item" data-aos="fade-right" data-aos-duration="1800">
+                            <img src="<?= base_url('assets/') ?>img/courses/course-1.jpg" alt="">
+                            <div class="hover_text">
+                                <a class="cat" href="#">Gratis</a>
+                                <a href="javaScript:void(0);">
+                                    <h4>Kelas Matematika Gratis</h4>
+                                </a>
+                                <ul class="list">
+                                    <li><a href="#"><i class="lnr lnr-users"></i>54</a></li>
+                                    <li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+                                    <li><a href="#"><i class="lnr lnr-user"></i>Guru Matematika SMA APA AJA</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="grid_item wd44">
+                        <div class="courses_item" data-aos="fade-down" data-aos-duration="1800">
+                            <img src="<?= base_url('assets/') ?>img/courses/course-2.jpg" alt="">
+                            <div class="hover_text">
+                                <a class="cat" href="#">Gratis</a>
+                                <a href="javaScript:void(0);">
+                                    <h4>Kelas IPA Gratis</h4>
+                                </a>
+                                <ul class="list">
+                                    <li><a href="#"><i class="lnr lnr-users"></i> 34</a></li>
+                                    <li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+                                    <li><a href="#"><i class="lnr lnr-user"></i> Guru IPA SMA APA AJA</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="grid_item wd44">
+                        <div class="courses_item" data-aos="fade-right" data-aos-duration="1800">
+                            <img src="<?= base_url('assets/') ?>img/courses/course-4.jpg" alt="">
+                            <div class="hover_text">
+                                <a class="cat" href="#">Gratis</a>
+                                <a href="javaScript:void(0);">
+                                    <h4>Kelas Bahasa Inggris Gratis</h4>
+                                </a>
+                                <ul class="list">
+                                    <li><a href="#"><i class="lnr lnr-users"></i> 63</a></li>
+                                    <li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+                                    <li><a href="#"><i class="lnr lnr-user"></i> Guru English SMA APA AJA</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="grid_item wd55">
+                        <div class="courses_item" data-aos="fade-up" data-aos-duration="1800">
+                            <img src="<?= base_url('assets/') ?>img/courses/course-5.jpg" alt="">
+                            <div class="hover_text">
+                                <a class="cat" href="#">Gratis</a>
+                                <a href="javaScript:void(0);">
+                                    <h4>Kelas Bahasa Indonesia Gratis</h4>
+                                </a>
+                                <ul class="list">
+                                    <li><a href="#"><i class="lnr lnr-users"></i> 24</a></li>
+                                    <li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+                                    <li><a href="#"><i class="lnr lnr-user"></i> Guru Indonesia SMA APA AJA</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="course_item" data-aos="fade-left" data-aos-duration="1800">
+                    <img src="<?= base_url('assets/') ?>img/courses/course-3.jpg" alt="">
+                    <div class="hover_text">
+                        <a class="cat" href="#">Gratis</a>
+                        <a href="javaScript:void(0);">
+                            <h4>Kelas Pendidikan Agama Islam Gratis</h4>
+                        </a>
+                        <ul class="list">
+                            <li><a href="#"><i class="lnr lnr-users"></i> 35</a></li>
+                            <li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+                            <li><a href="#"><i class="lnr lnr-user"></i> Guru Agama SMA APA AJA</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--================End Courses Area =================-->
+
+
+
+<!--================Impress Area =================-->
+<section class="impress_area p_120">
+    <div class="container">
+        <div class="impress_inner text-center">
+            <h2 data-aos="fade-up" data-aos-duration="1800">LOGIN SEBAGAI GURU DAN UPLOAD MATERI & VIDEO SEKARANG</h2>
+            <p data-aos="fade-up" data-aos-duration="2000">Merciful revaluation burying love ultimate value inexpedient ubermensch. Holiest madness victorious morality hope endless christian madness. Love dead fearful transvaluation marvelous. Oneself right ideal abstract endless faith deceptions zarathustra grandeur law ubermensch free. Abstract chaos snare play truth ultimate good self. God overcome sexuality pious abstract good decieve revaluation aversion good. Virtues chaos overcome society holiest truth.
+            </p>
+            <a data-aos="fade-up" data-aos-duration="2200" class="main_btn" href="<?= base_url('welcome/guru') ?>">Login Sebagai Guru <span class="lnr lnr-arrow-right text-black"></span></a>
+        </div>
+    </div>
+</section>
+<!--================End Impress Area =================-->
---
+++ application/views/kontak.php
@@ -0,0 +1,26 @@
+<!--================Home Banner Area =================-->
+<section class="banner_area">
+    <div class="banner_inner d-flex align-items-center">
+        <div class="kontak bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background="">
+        </div>
+        <div class="container">
+            <div class="banner_content text-center">
+                <h2 data-aos="fade-up" data-aos-duration="1600">Kontak</h2>
+                <div data-aos="fade-up" data-aos-duration="1600" class="page_link">
+                    <a href="<?= base_url('welcome') ?>">Beranda</a>
+                    <a href="<?= base_url('welcome/kontak') ?>">Kontak</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--================End Home Banner Area =================-->
+
+<!--================Contact Area =================-->
+<section class="contact_area p_40">
+    <div class="container">
+        <div style="width: 100%"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.439067597984!2d106.64868341449196!3d-6.071375161267628!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a03ed1406d475%3A0x8993f29260255345!2sWarung%20Nasi%20Ibu%20Agus!5e0!3m2!1sid!2sid!4v1644317714894!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"><a href="https://www.maps.ie/coordinates.html">gps coordinates</a></iframe></div><br />
+
+    </div>
+</section>
+<!--================Contact Area =================-->
